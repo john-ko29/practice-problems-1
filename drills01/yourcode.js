c46616b9ed6457788b80cb63e1064144c96abe35
John Ko <jko121790@gmail.com>
2020-05-21 11:26:34 -0700

    fitWithinVal exercise complete

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,23 @@
 
-function sumArray(){
-	
+function sumArray(array){
+  let total = 0;
+	for (let i = 0; i < array.length; i++) {
+    total += array[i]
+  }
+  return total;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, number){
+  let total = 0;
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const currentValue = array[i];
+    if ((total + currentValue) < number) {
+      total += currentValue;
+      newArray.push(currentValue)
+    }
+  }
+  return newArray;
 }
 
 function getAllNamesShorterThan(){
