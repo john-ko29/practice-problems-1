3a830982641c5720bd96b21c4336866cf96edc13
John Ko <jko121790@gmail.com>
2020-05-22 10:38:22 -0700

    countOccurance complete

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,7 +1,13 @@
 
 
-function countOccurences(){
-
+function countOccurences(arr, word){
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(word)) {
+      count++
+    }
+  }
+  return count;
 }
 
 function wordLengths(){
