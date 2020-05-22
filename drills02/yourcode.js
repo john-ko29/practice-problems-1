627bbfeb229e42444aea9d31be614cda4d432bad
John Ko <jko121790@gmail.com>
2020-05-22 10:55:46 -0700

    getMinMaxMean  complete

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -18,8 +18,25 @@ function wordLengths(arr){
   return newArr;
  }
 
-function getMinMaxMean(){
-
+function getMinMaxMean(arr){
+  const obj = {};
+  let min = Infinity;
+  let max = -Infinity;
+  let mean = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i]
+    }
+    if (max < arr[i]) {
+      max = arr[i]
+    }
+    mean += arr[i]
+  }
+  console.log(min, max, mean/arr.length)
+  obj.min = min;
+  obj.max = max;
+  obj.mean = mean/arr.length;
+  return obj;
 }
 
 function findMode(){
