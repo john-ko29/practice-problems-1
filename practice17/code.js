1389f1ab57878e8e9bbfe819751f17b657648dde
John Ko <jko121790@gmail.com>
2020-06-23 09:52:54 -0700

    practice 17 complete

--- practice17/code.js
+++ practice17/code.js
@@ -1,3 +1,14 @@
 function skippyNumbers(num) {
-
+  const arr = [];
+  let number = num;
+  arr.push(num);
+  for(let i = 0; i < 7; i++) {
+    if (i % 2 === 0) {
+      number += 1;
+    }  else {
+      number += 2;
+    }
+    arr.push(number);
+  }
+  return arr;
 }
